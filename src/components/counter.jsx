3888c8cd81c4fcbd38c5a647deef4b064ff4c3b4
Taher Barakat <taherbarakat12223@gmail.com>
2022-04-29 14:25:58 +0300

    Updating the state.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,40 +3,48 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        tags: ['tag1', 'tag2', 'tag3'],
+        // tags: ['tag1', 'tag2', 'tag3'],
     };
-    renderTags() {
-        if (this.state.tags.length === 0) return <p>no tags</p>;
-        return (
-            <ul>
-                {this.state.tags.map((tag) => (
-                    <li key={tag}> {tag}</li>
-                ))}
-            </ul>
-        );
-    }
+
+    handleIncrement = () => {
+        this.setState({ count: this.state.count + 1 });
+    };
+
+    // renderTags() {
+    //     if (this.state.tags.length === 0) return <p>no tags</p>;
+    //     return (
+    //         <ul>
+    //             {this.state.tags.map((tag) => (
+    //                 <li key={tag}> {tag}</li>
+    //             ))}
+    //         </ul>
+    //     );
+    // }
 
     render() {
         return (
             <div>
-                {/* <span className={this.getBadgeColor()}>
+                <span className={this.getBadgeColor()}>
                     {this.formatCount()}
                 </span>
-                <button className=" btn btn-secondary btn-sm">Increment</button> */}
-                {this.state.tags.length === 0 && <p>ll</p>}
-                {this.renderTags()}{' '}
+                <button
+                    onClick={this.handleIncrement}
+                    className=" btn btn-secondary btn-sm"
+                >
+                    Increment
+                </button>
             </div>
         );
     }
-    // getBadgeColor() {
-    //     let classes = 'badge  m-2 badge-';
-    //     classes += this.state.count === 0 ? 'warning' : 'primary';
-    //     return classes;
-    // }
+    getBadgeColor() {
+        let classes = 'badge  m-2 badge-';
+        classes += this.state.count === 0 ? 'warning' : 'primary';
+        return classes;
+    }
 
-    // formatCount() {
-    //     return this.state.count === 0 ? 'zero' : this.state.count;
-    // }
+    formatCount() {
+        return this.state.count === 0 ? 'zero' : this.state.count;
+    }
 }
 
 export default Counter;
